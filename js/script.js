09fd226793c1ace9ecdb28b5d0d3ec9cda26e4f6
dragoncartoon <quocvuongtphcm@yahoo.com>
2021-09-16 17:56:36 +0700

    ver 1.0

---
+++ js/script.js
@@ -0,0 +1,119 @@
+/*
+TODO: 
+x Random Robot choose. 
+- Human choose by click button (later). 
+-  Click Rock button, show 'rock'. Then run playRound()
+x Compare Human choose vs Robot. Rules Rock < Paper < Scissors < Rock.
+x   If Robot or Human win, +1 score with some display text about winner.
+x When score reach 5, display the winner.
+- Reset game button and function
+
+Game step break
+1. User choose Rock OR Paper OR scissors by click button.
+2. Compare user choose and system choose.
+3. If user win, user score +1 & win text. If system win, +1 score + win text. If tie, show tie.
+4. Whoever score reach 5. Game stop, display winner.
+5. User can click Reset button to reset. 
+
+*/
+
+const gameButton = document.querySelectorAll(".game-button");
+const rockBtn = document.getElementsByClassName("rock");
+const paperBtn = document.getElementsByClassName("paper");
+const scissorsBtn = document.getElementsByClassName("scissors");
+
+let humanScore = 0;
+let robotScore = 0;
+// let roundWinner = '';
+
+// UI
+const playerScore = document.getElementById("playerScore");
+const machineScore = document.getElementById("robotScore");
+const robotChoose = document.getElementById("robotChoose");
+const round = document.getElementById("round");
+const buttonPlayAgain = document.querySelector(".play-again");
+
+function robotPlay() {
+  const items = ["rock", "paper", "scissors"];
+  const robotSelection = items[Math.floor(Math.random() * items.length)];
+  return robotSelection;
+}
+
+rockBtn[0].addEventListener("click", () => handleClick("rock"));
+paperBtn[0].addEventListener("click", () => handleClick("paper"));
+scissorsBtn[0].addEventListener("click", () => handleClick("scissors"));
+
+// Need some way to repeat playRound()
+
+function playRound(humanSelection, robotSelection) {
+  console.log(humanSelection, robotSelection);
+
+  if (humanSelection === robotSelection) {
+    round.innerText = "So close";
+  } else if (
+    (humanSelection === "rock" && robotSelection === "paper") ||
+    (humanSelection === "paper" && robotSelection === "scissors") ||
+    (humanSelection === "scissors" && robotSelection === "rock")
+  ) {
+    // roundWinner = 'robot';
+    robotScore++;
+    round.innerText = "Argh, one more";
+  } else if (
+    (robotSelection === "rock" && humanSelection === "paper") ||
+    (robotSelection === "paper" && humanSelection === "scissors") ||
+    (robotSelection === "scissors" && humanSelection === "rock")
+  ) {
+    // roundWinner = 'human';
+    humanScore++;
+    round.innerText = "You win this round";
+  }
+}
+
+function disableGameButton() {
+  gameButton.forEach((btn) => {
+    btn.disabled = true;
+  });
+}
+
+function handleClick(humanSelect) {
+  // console.log(`${humanSelect}, ${humanScore} `)
+  robotSelection = robotPlay();
+  humanSelection = humanSelect;
+  playRound(humanSelection, robotSelection);
+  checkScore();
+}
+
+// TODO: Function check score here
+function winnerAnnouncement(winner) {
+  if ((winner = "human")) {
+    round.innerText = "You saved the world";
+  } else {
+    round.innerText = "It's the end";
+  }
+}
+
+function checkScore() {
+  if (humanScore === 5) {
+    winnerAnnouncement("human");
+    disableGameButton();
+    buttonPlayAgain.style.visibility = "visible";
+  } else if (robotScore === 5) {
+    winnerAnnouncement("robot");
+    disableGameButton();
+    buttonPlayAgain.style.visibility = "visible";
+  } else {
+    updateUI();
+  }
+}
+
+function updateUI() {
+  playerScore.textContent = `The last hope: ${humanScore}`;
+  machineScore.textContent = `The Robot: ${robotScore}`;
+  robotChoose.textContent = `Robot choose: ${robotSelection}`;
+}
+
+function resetGame() {
+  buttonPlayAgain.addEventListener("click", () => {
+    location.reload();
+  });
+}
